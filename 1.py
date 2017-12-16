2a1b4ff167ceb21533a268d670b55b2f5b5cd06c
Code4Learn <code4learn6@gmail.com>
2017-12-16 02:23:25 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+username = 'mahmoud'
+password = '12345'
+
+
+if username == 'mahmoud' and password == '12345':
+    print('login success , welcome mahmoud')
+
+
+if username == 'ahmed' or password == '12345':
+    print('login success , welcome ahmed')
+
+
+
+# and 2 Conditions = True
+# or 1 conditions = True
